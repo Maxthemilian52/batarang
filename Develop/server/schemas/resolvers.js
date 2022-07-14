a463dad38379f5fe7ec559f6d9b2cc67d350a4f5
Maximilian Shaw <maximilianyoung52@gmail.com>
2022-07-14 13:18:01 -0400

    working on typedefs

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,19 +1,24 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Comment, Event, Post, User } = require('../models');
+const { Event, Post, User } = require('../models');
 
 const resolvers = {
     Query: {
       users: async () => {
         return User.find({});
-    },
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username }).populate('posts')
+      },
       event: async () => {
         return Event.find({});
-    },
-      comment: async () => {
-        return Post.find({});
-    },
+      },
+      comment: async (parent, { postId }, context) => {
+        if(context.user) {
+          return Post.findOne({ _id: postId })
+        }
+     },
       post: async () => {
-        return Comment.find({});
+        return Post.find({});
     },
 }
 };
